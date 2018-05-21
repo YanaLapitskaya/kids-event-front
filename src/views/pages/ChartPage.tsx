cf578db37fefcf1dcf9036604358baa216072406
Yana Lapitskaya <y.lapitskaya@hackspace.by>
2018-05-22 01:18:15 +0300

    finished profit chart

--- src/views/pages/ChartPage.tsx
+++ src/views/pages/ChartPage.tsx
@@ -12,7 +12,8 @@ charts(fusioncharts);
 
 class ChartPage extends React.Component<any, any> {
     state = {
-        groupedProfit: []
+        groupedProfit: [],
+        timeInterval: 0
     };
 
     chartConfigs = {
@@ -22,7 +23,7 @@ class ChartPage extends React.Component<any, any> {
         dataSource: {
             chart: {
                 caption: 'Выручка за период',
-                xAxisName: 'Дата оформления заказа, дд.мм',
+                xAxisName: 'Дата оформления заказа, дд/мм',
                 yAxisName: 'Выручка, бел. руб.'
             },
             data: []
@@ -30,39 +31,91 @@ class ChartPage extends React.Component<any, any> {
     };
 
     render() {
-        let groupedByDate  = [];
+        let groupedProfit = [];
         if ( this.props.orders.length !== 0 && this.state.groupedProfit.length === 0 ) {
+            const processedOrders = this.props.orders.filter(ord => ord.status === 'обработан')
+                .map((el) => {
+                    el.dateOrder = new Date(el.dateOrder);
+                    return el;
+                })
+                .sort((a, b) => {
+                    return a.dateOrder - b.dateOrder;
+                });
+
+            const timeInterval = (processedOrders[processedOrders.length - 1].dateOrder - processedOrders[0].dateOrder) / (1000 * 60 * 60 * 24);
+
             let groupedProfitObj = {};
-            groupedByDate = this.props.orders.map((ord) => {
-                const dateString = ord.dateOrder;
-                const date = new Date(dateString);
-                return [`${date.getDate()}.${date.getMonth() + 1}`, ord];
-            });
-            groupedByDate.map((el) => {
-                if (!groupedProfitObj[el[0]]) {
-                    groupedProfitObj[el[0]] = Number(el[1].price);
+
+            processedOrders.map((el) => {
+                const dateString = `${el.dateOrder.getDate()}/${el.dateOrder.getMonth() + 1}`;
+                if (!groupedProfitObj[dateString]) {
+                    groupedProfitObj[dateString] = Number(el.price);
                 } else {
-                    groupedProfitObj[el[0]] += Number(el[1].price);
+                    groupedProfitObj[dateString] += Number(el.price);
                 }
             });
-            const groupedProfit: Array<Object> = Object.keys(groupedProfitObj).sort().map((key) => {
+
+            groupedProfit = Object.keys(groupedProfitObj).map((key) => {
                 return {label: key, value: groupedProfitObj[key]};
             });
+
             this.setState({
-                groupedProfit: groupedProfit
+                groupedProfit: groupedProfit,
+                timeInterval: timeInterval
             });
             this.chartConfigs.dataSource.data = groupedProfit;
         }
+
+        const title = this.state.groupedProfit.length !== 0 ? 
+            <h1 {...header}>Статистика за период: {this.state.groupedProfit[0].label} - {this.state.groupedProfit[this.state.groupedProfit.length - 1].label}</h1>
+            : null;
+        
+        let chartText = null;
+        if (this.state.groupedProfit.length !== 0) {
+            let totalProfit = this.state.groupedProfit.reduce((sum, a) => {  return sum + Number(a.value); }, 0);
+            let averageProfit = Math.floor(totalProfit / Math.floor(this.state.timeInterval));
+            chartText = (<div {...textStyles}>
+                <p>Выручка за период: <br/> <b>{totalProfit} бел. руб.</b></p>
+                <p>Средняя выручка: <br/> <b>{averageProfit} бел. руб.</b></p>
+            </div>);
+        }
+
         return (
             <div>
                 <SideMenu {...this.props}/>
-                <div>
-                    <ReactFC {...this.chartConfigs} />
-                    <p>Выручка за период: {profit}</p> 
+                <div {...wrapper}>
+                    {title}
+                    <div {...chartWrapper}>
+                        <ReactFC {...this.chartConfigs} />
+                        {chartText}
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
+const header = css({
+    fontFamily: 'Roboto !important',
+    marginBottom: '25px'
+});
+
+const wrapper = css({
+    paddingTop: '25px',
+    ' p': {
+        fontFamily: 'Roboto !important'
+    }
+});
+
+const chartWrapper = css({
+    display: 'flex'
+});
+
+const textStyles = css({
+    width: '30%',
+    display: 'inline-block',
+    fontSize: '30px',
+    margin: '20px 40px'
+});
 
 export default ChartPage;
