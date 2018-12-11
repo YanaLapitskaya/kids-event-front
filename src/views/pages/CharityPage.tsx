da70e20c696f7f34cb58355ac24343c60d1e0f49
Lapitskaya Yana <Lapitskaya_YV@itd.iba.by>
2018-12-11 19:37:07 +0300

    added charity page

--- src/views/pages/CharityPage.tsx
+++ src/views/pages/CharityPage.tsx
@@ -7,7 +7,11 @@ class CharityPage extends React.Component<any, any> {
         return (
             <BasePage>
                 <div {...styles}>
-                    Следите за новостями в социальных сетях! :)
+                    Наша дружная семья KidsEvent хотела бы подарить частичку души каждому ребенку в этой стране и не только. <br/> 
+                    Особенно тем, у кого нет возможности посещать праздничные меропртиятия. <br/> 
+                    Мы говорим о детским домах, интернатах и домах молюток. <br/> 
+                    На вырученные средства с каждого праздника мы готовы радовать деток своими программами, подарками и хорошим настроением :) <br/> 
+                    Фотоотчеты будут появляться очень скоро у нас на сайте в разделе <b>«Фото»</b> и в нашем инстаграме <b>Kids_event.by</b>.
                 </div>
             </BasePage>
         );
@@ -16,7 +20,10 @@ class CharityPage extends React.Component<any, any> {
 
 const styles = css({
     padding: '30px 50px',
-    height: '100%'
+    height: '100%',
+    textAlign: 'center',
+    fontSize: '1.2rem'
+
 });
 
 export default CharityPage;
